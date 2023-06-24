dfd26885df39ceee8e7d32df46de8f618df3deac
swapnil-sh <swapnil.sharma2709@gmail.com>
2023-06-24 21:27:18 +0530

    initial commit

---
+++ creational-design-patterns/factory-design-pattern/src/main/java/oop/dp/java/factorydesignpattern/FactoryDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package oop.dp.java.factorydesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryDesignPatternApplication.class, args);
+	}
+
+}
